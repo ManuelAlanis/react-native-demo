f7913d22d8168e0f2b80fc828ca94875f42c18d2
Manuel Alanis <man.alaniz@gmail.com>
2019-03-29 17:35:28 -0700

    my first commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,33 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // make a component
 const Header = () => {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
 
-    return <Text style={textStyle}>HEADER</Text>;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>HEADER</Text>
+        </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+      backgroundColor: '#ececec',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 60,
+      paddingTop: 5,
+      shadowColor: '#8989ff',
+      shadowOffset: { width: 0, height: 20 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
+    },
     textStyle: {
-        fontSize: 40,
+        fontSize: 20,
     }
 };
 
